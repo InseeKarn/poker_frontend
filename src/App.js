4f40b542c9bb1e0d6b5021bed7531a1448853a4a
inseekarn <inseekan@gmail.com>
2024-10-01 17:08:10 +0700

    ?

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import PokerTable from './components/PokerTable';
 
 function App() {
   return (
---
+++ src/components/PokerTable.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './PokerTable.css';
+
+const PokerTable = () => {
+    return (
+        <div className="poker-table">
+            {/* วางโค้ดของตารางโป๊กเกอร์ที่นี่ */}
+            <h1>Welcome to Online Poker!</h1>
+        </div>
+    );
+};
+
+export default PokerTable;
